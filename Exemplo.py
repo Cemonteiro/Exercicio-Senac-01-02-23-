a6f9c992c808ac9bb344d2529f60c69d8450b108
Eduardo <ceduardorm2000@gmail.com>
2023-01-31 20:56:53 -0300

    altereção2

--- Exemplo.py
+++ Exemplo.py
@@ -5,6 +5,9 @@ class Produto:
         self.nome = nome
         self.preco = preco
         self.estoque = estoque
+    def __init__(self):
+        print(f"Nome: {self.nome} -Preco: {self.preco}")
+        
 produto = Produto("Massa de pizaa",5,400)
 quantidade = int(input("Informe a quantidade: "))
 print(quantidade)
